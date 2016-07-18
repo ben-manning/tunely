1b33b439f8a6baa8b4e1510f91ace44303e70b65
Ben Manning <bmanning@me.com>
2016-07-18 16:35:03 -0700

    stopping point until this evening

--- models/album.js
+++ models/album.js
@@ -1,2 +1,10 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+albums.push({
+              _id: 132,
+              artistName: 'Nine Inch Nails',
+              name: 'The Downward Spiral',
+              releaseDate: '1994, March 8',
+              genres: [ 'industrial', 'industrial metal' ]
+            });
