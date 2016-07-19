cd73d4f498116b5fd2e5aa2b039a6f4f11e677db
Ben Manning <bmanning@me.com>
2016-07-18 21:24:46 -0700

    connect api/ajax/front-end/mongo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,15 +4,23 @@
  * into functions and objects as needed.
  *
  */
-
-
 $(document).ready(function() {
   console.log('app.js loaded!');
-  // render the hard-coded data list
-  sampleAlbums.forEach(renderAlbum);
 
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/api/albums',
+    dataType: 'json',
+    success: onSuccess
+  });
+
+  function onSuccess(albums) {
+    var records = albums.albums;
+    records.forEach(function(album) {
+      renderAlbum(album);
+    });
+  }
 });
-
 
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
@@ -20,16 +28,4 @@ function renderAlbum(album) {
   var templateFunction = Handlebars.compile(albumHtml);
   var html = templateFunction(album);
   $("#albums").prepend(html);
-}
-
-// ajax call to get the albums
-$.ajax({
-  method: 'GET',
-  url: 'https://localhost:3000/api/albums',
-  dataType: 'json',
-  success: onSuccess
-});
-
-function onSuccess(json) {
-    console.log(json);
 }
