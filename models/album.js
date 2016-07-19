2641d5d32a362912b1d2ebcf6b06042ca31dc7e9
Ben Manning <bmanning@me.com>
2016-07-18 19:54:54 -0700

    add seeded data to seed.js

--- models/album.js
+++ models/album.js
@@ -1,10 +1,13 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-albums.push({
-              _id: 132,
-              artistName: 'Nine Inch Nails',
-              name: 'The Downward Spiral',
-              releaseDate: '1994, March 8',
-              genres: [ 'industrial', 'industrial metal' ]
-            });
+var AlbumSchema = new Schema({
+  artistName: String,
+  name: String,
+  releaseDate: String,
+  genres: [ String ]
+});
+
+var Album = mongoose.model('Album', AlbumSchema);
+
+module.exports = Album;
